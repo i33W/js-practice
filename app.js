9ecc02f33a5b87e34893122be19ccdc1d5623aed
i33W <hwangbomee@kakao.com>
2022-09-21 17:27:38 +0900

    06. 페이징 구현하기

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = { currentPage: 1 };
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -16,31 +17,43 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
   newsList.push("<ul>");
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
     <li>
-        <a href="#${newsFeed[i].id}">
+        <a href="#/show/${newsFeed[i].id}">
             ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
     </li>
   `);
   }
   newsList.push("</ul>");
-
+  // 페이징
+  newsList.push(`
+    <div>
+        <a href="#/page/${
+          store.currentPage > 1 ? store.currentPage - 1 : 1
+        }">이전 페이지</a>
+        <a href="#/page/${
+          store.currentPage + 1 > Math.ceil(newsFeed.length / 10)
+            ? Math.ceil(newsFeed.length / 10)
+            : store.currentPage + 1
+        }">다음 페이지</a>
+    </div>
+    `);
   // root에 추가
   container.innerHTML = newsList.join("");
 }
 
 // 목록에서 클릭했을 때, 상세 페이지
 function newsDetail() {
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   // root에 추가
   container.innerHTML = `
         <h1>${newsContent.title}</h1>
 
-        <div><a href="#">목록으로</a></div>
+        <div><a href="#/page/${store.currentPage}">목록으로</a></div>
     `;
 }
 
@@ -49,6 +62,9 @@ function router() {
   const routePath = location.hash;
 
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
